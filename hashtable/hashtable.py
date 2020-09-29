015a557e0fb10d2114be0f15f55677916afc37d4
adriana <atoledosantiago1@gmail.com>
2020-09-28 22:11:01 -0500

    mvp day one

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -21,7 +21,8 @@ class HashTable:
     """
 
     def __init__(self, capacity):
-        # Your code here
+        self.capacity = capacity
+        self.table = [None] * capacity
 
 
     def get_num_slots(self):
@@ -34,7 +35,7 @@ class HashTable:
 
         Implement this.
         """
-        # Your code here
+        return self.capacity
 
 
     def get_load_factor(self):
@@ -43,7 +44,7 @@ class HashTable:
 
         Implement this.
         """
-        # Your code here
+        return self.size/self.capacity
 
 
     def fnv1(self, key):
@@ -62,7 +63,11 @@ class HashTable:
 
         Implement this, and/or FNV-1.
         """
-        # Your code here
+        hash = 5381
+        for x in key:
+            hash = ((hash << 5) + hash) + ord(x)
+        return hash
+
 
 
     def hash_index(self, key):
@@ -81,7 +86,9 @@ class HashTable:
 
         Implement this.
         """
-        # Your code here
+        index = self.hash_index(key)
+        self.table[index] = value
+
 
 
     def delete(self, key):
@@ -92,7 +99,8 @@ class HashTable:
 
         Implement this.
         """
-        # Your code here
+        index = self.hash_index(key)
+        self.table[index] = None
 
 
     def get(self, key):
@@ -103,7 +111,12 @@ class HashTable:
 
         Implement this.
         """
-        # Your code here
+        index = self.hash_index(key)
+
+        if (self.table[index]):
+            return self.table[index]
+        else:
+            return None
 
 
     def resize(self, new_capacity):
