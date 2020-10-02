06e39753140ed5f1af39289ccb9942cba2c82ba3
adriana <atoledosantiago1@gmail.com>
2020-10-02 13:37:34 -0500

    mvp

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,33 @@
 # Your code here
 
+with open("robin.txt") as f:
+    words = f.read()
+
+def word_count(s):
+    # Your code here
+
+    punctuation = ['"', ':', ';', ',', '.', '-', '+', '=', '/', '\\', '|', '[', ']', '{', '}', '(', ')', '*', '^', '&']
+
+    for x in punctuation:
+        s = s.replace(x, '')
+
+    words = s.split()
+
+    dict = {}
+
+    for word in words:
+        if word.lower() in dict:
+            dict[word.lower()] += 1
+        else:
+            dict[word.lower()] = 1
+
+    return dict
+
+word_dict = word_count(words)
+max_space = max(len(x) for x in word_dict)
+
+sorted_dict = sorted(word_dict.items(), key=lambda x: x[1], reverse = True)
+
+
+for x in sorted_dict:
+    print(x[0], " " * (max_space - len(x[0])), "#" * x[1])
--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -1,13 +1,41 @@
 import random
 
-# Read in all the words in one go
+
 with open("input.txt") as f:
     words = f.read()
 
-# TODO: analyze which words can follow other words
-# Your code here
 
 
-# TODO: construct 5 random sentences
-# Your code here
+words = words.split()
+start_words = []
+end_words = []
+d = {}
 
+for x in range(len(words) - 1):
+    if not d.get(words[x]):
+        d[words[x]] = [words[x + 1]]
+    else:
+        d[words[x]].append(words[x + 1])
+
+for x in words:
+    if x[0].isupper():
+        start_words.append(x)
+    if x == '"' and x[1].isupper():
+        start_words.append(x)
+    if x.endswith(("!", "?", ".")):
+        end_words.append(x)
+    if x.endswith('"'):
+        if x[:-1].endswith(("!", "?", ".")):
+            end_words.append(x)
+
+for i in range(5):
+    word_print = random.choice(start_words)
+    stop = False
+    while not stop:
+        print(word_print, end=" ")
+        if word_print in end_words:
+            stop = True
+            break
+        else:
+            word_print = random.choice(d[word_print])
+    print("\n")
